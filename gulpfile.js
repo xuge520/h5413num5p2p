a356c6947738574f8e2c9163e10f64a0d0ea58a4
苟先生 <584416334@qq.com>
2018-08-05 15:30:33 +0800

    首页

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var gulp=require("gulp");
 
 //引入第三方模块
 var less=require("gulp-less");
-var sass = require('gulp-sass');
 var path=require("path");
 var cleanCSS=require("gulp-clean-css");
 var rename=require("gulp-rename");
@@ -18,13 +17,6 @@ gulp.task("lestTask",function () {
 	.pipe(gulp.dest("dist/css")); //输出目录
 });
 
-//配置sass编译任务
-gulp.task("sassTask",function () {
-	gulp.src("src/sass/*.scss") //源目录
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest("dist/css")); //输出目录
-});
-
 //配置压缩css的任务
 gulp.task("minCssTask",function () {
 	gulp.src("dist/css/*.css") //源目录
@@ -32,7 +24,7 @@ gulp.task("minCssTask",function () {
 	.pipe(rename({
 		suffix:".min" //重命名加min后缀
 	}))
-	.pipe(gulp.dest("dist/css/")); //输出目录
+	.pipe(gulp.dest("dist/css/min")); //输出目录
 });
 
 //配置js压缩的任务
@@ -48,7 +40,6 @@ gulp.task("minJsTask",function () {
 //开启观察者watch
 gulp.task("default",function () {
 	gulp.watch("src/less/*.less",["lestTask"]); //观察less变化执行less编译任务
-	gulp.watch("src/sass/*.scss",["sassTask"]); //观察sass变化执行sass编译任务
 	gulp.watch("dist/css/*.css",["minCssTask"]); //观察css变化执行css压缩任务
 	gulp.watch("src/javascript/*.js",["minJsTask"]); //观察js变化执行js压缩任务
 });
---
+++ src/javascript/index.js
@@ -0,0 +1,3 @@
+$(function () {
+    $('#contanctus').tooltip()
+});
